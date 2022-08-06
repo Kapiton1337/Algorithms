a51ced2f410c609ac6517160e233ca6111ded83d
Kapiton2.0 <Polevtsuv63@mgmail.ru>
2022-08-06 23:20:39 +0600

    upgrade romanToInt

--- romanToInt/romanToInt/romanToInt.cpp
+++ romanToInt/romanToInt/romanToInt.cpp
@@ -5,24 +5,114 @@ using namespace std;
 class Solution {
 public:
     int romanToInt(string s) {
-        for (auto iter = s.begin(); iter < s.end(); iter++) {
-            switch (*iter)
+        switch (*s.begin())
+        {
+        case 'I': {
+            this->answer++;
+            break;
+        }
+        case 'V': {
+            this->answer += 5;
+            break;
+        }
+        case 'X': {
+            this->answer += 10;
+            break;
+        }
+        case 'L': {
+            this->answer += 50;
+            break;
+        }
+        case 'C': {
+            this->answer += 100;
+            break;
+        }
+        case 'D': {
+            this->answer += 500;
+            break;
+        }
+        case 'M': {
+            this->answer += 1000;
+            break;
+        }
+        default:
+            break;
+        }
+        for (auto iter = s.begin() + 1; iter < s.end(); iter++) {
+            current = *iter;
+            previous = *(iter - 1);
+            switch (current)
             {
-            case 'I':
+            case 'I': {
                 this->answer++;
-            case 'V':
-                this->answer += 5;
-            case 'X':
-                this->answer += 10;
-            case 'L':
-                this->answer += 50;
-            case 'C':
-                this->answer += 100;
-            case 'D':
-                this->answer += 500;
-            case 'M':
-                this->answer += 1000;
-
+                break;
+            }
+            case 'V': {
+                if (previous == 'I')
+                {
+                    this->answer += 3;
+                }
+                else
+                {
+                    this->answer += 5;
+                }
+                break;
+            }
+            case 'X': {
+                if (previous == 'I')
+                {
+                    this->answer += 8;
+                }
+                else
+                {
+                    this->answer += 10;
+                }
+                break;
+            }
+            case 'L': {
+                if (previous == 'X')
+                {
+                    this->answer += 30;
+                }
+                else
+                {
+                    this->answer += 50;
+                }
+                break;
+            }
+            case 'C': {
+                if (previous == 'X')
+                {
+                    this->answer += 80;
+                }
+                else
+                {
+                    this->answer += 100;
+                }
+                break;
+            }
+            case 'D': {
+                if (previous == 'C')
+                {
+                    this->answer += 300;
+                }
+                else
+                {
+                    this->answer += 500;
+                }
+                break;
+            }
+            case 'M': {
+                if (previous == 'C')
+                {
+                    this->answer += 800;
+                }
+                else
+                {
+                    this->answer += 1000;
+                }
+                break;
+            }
             default:
                 break;
             }
@@ -31,12 +121,16 @@ public:
     }
 private:
     int answer = 0;
+    char current;
+    char previous;
 };
 
 int main()
 {
+    string s;
     Solution a;
-    int b = a.romanToInt("III");
-    cout << b;
+    cin >> s;
+    int b = a.romanToInt(s);
+    cout << b << endl;
 }
 
