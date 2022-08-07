7730eb4422770834eb734eea3e57240bb4d31b94
Kapiton2.0 <Polevtsuv63@mgmail.ru>
2022-08-07 13:39:05 +0600

    romeToInt success

--- romanToInt/romanToInt/romanToInt.cpp
+++ romanToInt/romanToInt/romanToInt.cpp
@@ -1,128 +1,37 @@
 ﻿#include <iostream>
+#include <map>
+#include <string>
 
 using namespace std;
 
 class Solution {
 public:
     int romanToInt(string s) {
-        switch (*s.begin())
+        map<char, int> romeNum;
+        romeNum['I'] = 1;
+        romeNum['V'] = 5;
+        romeNum['X'] = 10;
+        romeNum['L'] = 50;
+        romeNum['C'] = 100;
+        romeNum['D'] = 500;
+        romeNum['M'] = 1000;
+
+        answer = romeNum[*s.begin()];
+
+        for (auto iter = s.begin() + 1; iter != s.end(); iter++)
         {
-        case 'I': {
-            this->answer++;
-            break;
+            char previous = *(iter - 1);
+            char current = *iter;
+            if (romeNum[current] > romeNum[previous])
+                answer += romeNum[current] - 2 * romeNum[previous];
+            else
+                answer += romeNum[current];
         }
-        case 'V': {
-            this->answer += 5;
-            break;
-        }
-        case 'X': {
-            this->answer += 10;
-            break;
-        }
-        case 'L': {
-            this->answer += 50;
-            break;
-        }
-        case 'C': {
-            this->answer += 100;
-            break;
-        }
-        case 'D': {
-            this->answer += 500;
-            break;
-        }
-        case 'M': {
-            this->answer += 1000;
-            break;
-        }
-        default:
-            break;
-        }
-        for (auto iter = s.begin() + 1; iter < s.end(); iter++) {
-            current = *iter;
-            previous = *(iter - 1);
-            switch (current)
-            {
-            case 'I': {
-                this->answer++;
-                break;
-            }
-            case 'V': {
-                if (previous == 'I')
-                {
-                    this->answer += 3;
-                }
-                else
-                {
-                    this->answer += 5;
-                }
-                break;
-            }
-            case 'X': {
-                if (previous == 'I')
-                {
-                    this->answer += 8;
-                }
-                else
-                {
-                    this->answer += 10;
-                }
-                break;
-            }
-            case 'L': {
-                if (previous == 'X')
-                {
-                    this->answer += 30;
-                }
-                else
-                {
-                    this->answer += 50;
-                }
-                break;
-            }
-            case 'C': {
-                if (previous == 'X')
-                {
-                    this->answer += 80;
-                }
-                else
-                {
-                    this->answer += 100;
-                }
-                break;
-            }
-            case 'D': {
-                if (previous == 'C')
-                {
-                    this->answer += 300;
-                }
-                else
-                {
-                    this->answer += 500;
-                }
-                break;
-            }
-            case 'M': {
-                if (previous == 'C')
-                {
-                    this->answer += 800;
-                }
-                else
-                {
-                    this->answer += 1000;
-                }
-                break;
-            }
-            default:
-                break;
-            }
-        }
-        return this->answer;
+        return answer;
     }
+        
 private:
-    int answer = 0;
-    char current;
-    char previous;
+    int answer;
 };
 
 int main()
